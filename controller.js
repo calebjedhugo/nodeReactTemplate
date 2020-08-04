7373310b6bcaefbf52b65ab7d8626a6bfa443997
Caleb Hugo <caleb@calebhugo.com>
2020-08-04 13:39:54 -0400

    Should be all set. I'm sure I'll have to tweak something...always do...

--- controller.js
+++ controller.js
@@ -1,4 +1,7 @@
 const router = require('express').Router()
+const fs = require('fs')
+const path = require('path')
+const root = path.dirname(require.main.filename)
 
 //log all hits to the api.
 router.use((req, res, next) => {
@@ -9,8 +12,20 @@ router.use((req, res, next) => {
 //Send all requests through all middleware.
 router.use(require('./middleware'))
 
-//Do we have what they are looking for?
+//Handle api requests
 router.use('/api/v1', require('./routes'))
+
+//Handle DOM requests
+router.use(/\/.*/, (req, res, next) => {
+  const filePath = req.originalUrl === '/' ?
+    path.join(root, 'build/index.html') :
+    path.join(root, 'build', req.originalUrl)
+  if(fs.existsSync(filePath)){
+    res.sendFile(filePath)
+  } else {
+    next() //Send the 404 error.
+  }
+})
 
 //If they made it this far, it's time for that 404.
 router.use((req, res, next) => {
